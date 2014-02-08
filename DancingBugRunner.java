7652c8afe50824b4eb208c53e5a5c9479a10327c
Daniel Leonard <danielgleonard@outlook.com>
2014-02-07 19:01:49 -0600

    Prepared GridWorld code and added Jumper
    Imported GridWord code into the directory and added the Jumper class,
    though empty. Also edited .gitignore to be nicer.

---
+++ DancingBugRunner.java
@@ -0,0 +1,38 @@
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+
+/**
+ * This class runs a world that contains box bugs. <br />
+ * This class is not tested on the AP CS A and AB exams.
+ */
+public class DancingBugRunner
+{
+    public static void main(String[] args)
+    {
+        int[] dance1;
+        int[] dance2;
+        
+        dance1 = new int[4];
+        dance1[0] = 90;
+        dance1[1] = 135;
+        dance1[2] = 45;
+        dance1[3] = 315;
+        
+        dance2 = new int[5];
+        dance2[0] = 270;
+        dance2[1] = 225;
+        dance2[2] = 180;
+        dance2[3] = 0;
+        dance2[4] = 135;
+        
+        ActorWorld world = new ActorWorld();
+        DancingBug alice = new DancingBug(dance1);
+        alice.setColor(Color.ORANGE);
+        DancingBug bob = new DancingBug(dance2);
+        world.add(new Location(7, 8), alice);
+        world.add(new Location(5, 5), bob);
+        world.show();
+    }
+}
---
+++ ZBug.java
@@ -0,0 +1,51 @@
+import info.gridworld.actor.Bug;
+
+/**
+ * A <code>BoxBug</code> traces out a square "box" of a given size. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class ZBug extends Bug
+{
+    private int steps;
+    private int length;
+    private int section;
+
+    /**
+     * Constructs a box bug that traces a square of a given side length
+     * @param length the side length
+     */
+    public ZBug(int length)
+    {
+        steps = 0;
+        section = 1;
+        this.length = length;
+        setDirection(90);
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+    public void act()
+    {
+        if (steps < length && canMove())
+        {
+            move();
+            steps++;
+            return;
+        }
+        else if (steps == length && section == 2)
+        {
+            setDirection(90);
+            steps = 0;
+            section++;
+            return;
+        }
+        else if (steps == length && section == 1)
+        {
+            setDirection(225);
+            steps = 0;
+            section++;
+            return;
+        }
+    }
+}
