08cc71ba39ffc51d6d08147827a7e63ac0d707bb
Jefferson Henry <jefferson@yefferson.com>
2014-02-08 21:48:36 -0600

    Finished Jumper and added JumperRunner
    Jumper is an Actor that jumps ahead two places if it can; if not, it
    turns.
    JumperRunner constructs an ActorWorld and adds a Jumper and a Rock to
    the world.

--- Jumper.java
+++ Jumper.java
@@ -1,15 +1,91 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
 /**
  * The Jumper bug moves forward two spaces when it moves. It can move over rocks and flowers directly in front of it. It does not leave anything behind when it jumps.
  * 
  * @author Dan Leonard
  * @author Jefferson Henry
- * @author Chewy
+ * @author Matt Kramer
  * @author Jason Hanselman
  * 
- * @version 2
+ * @version 1.0.0
  */
 
-public class Jumper extends GridWorld_Classes.Bug
+public class Jumper extends Actor
 {
+    /**
+     * A bug that will jump.
+     */
+    public Jumper()
+    {
+        setColor(Color.RED);
+    }
     
+    /**
+     * A bug that will jump with a given color.
+     * @param color the color for this Bug
+     */
+    public Jumper(Color color)
+    {
+        setColor(color);
+    }
+    
+    /**
+     * Makes the bug act. If it can jump, it jumps. If not, it turns.
+     */
+    public void act()
+    {
+        if (canJump())
+            jump();
+        else
+            turn();
+    }
+    
+    /**
+     * Turns the Jumper half a right turn.
+     */
+    public void turn()
+    {
+        setDirection(getDirection() + Location.HALF_RIGHT);
+    }
+    
+    /**
+     * Moves the Jumper foward twice.
+     */
+    public void jump()
+    {
+        Grid<Actor> grid = getGrid();
+        Location currentLoc = getLocation();
+        Location oneNext = currentLoc.getAdjacentLocation(getDirection());
+        Location twoNext = oneNext.getAdjacentLocation(getDirection());
+        
+        if (grid.isValid(twoNext))
+            moveTo(twoNext);
+        else
+            turn();
+    }
+    
+    /**
+     * Tests if the Jumper can move forward two spots.
+     */
+    public boolean canJump()
+    {
+        Grid<Actor> grid = getGrid();
+        Location currentLoc = getLocation();
+        Location oneNext = currentLoc.getAdjacentLocation(getDirection());
+        Location twoNext = oneNext.getAdjacentLocation(getDirection());
+        
+        if (!grid.isValid(oneNext))
+            return false;
+        if (!grid.isValid(twoNext))
+            return false;
+        if (grid.get(twoNext) instanceof Flower)
+            return false;
+        return true;
+    }
 }
